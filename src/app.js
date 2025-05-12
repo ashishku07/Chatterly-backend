7e524229b3e78be2a493e1eef5b0d64ae78ecd98
shams261 <shamsshoaib261@gmail.com>
2025-05-11 20:40:07 -0400

    initial v1.02

--- src/app.js
+++ src/app.js
@@ -49,12 +49,21 @@ app.delete("/user", async (req, res) => {
   const userId = req.body.userId;
   try {
     const user = await User.findByIdAndDelete({ _id: userId });
-
-    //const user = await User.findByIdAndDelete({_id:userId});
-
     res.send("User deleted successfully");
   } catch (err) {
-    res.status(400).send("Something went wrong" + err.message);
+    res.status(400).send("Something went wrong: ");
+  }
+});
+
+// patch the user api call
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    await User.findByIdAndUpdate({ _id: userId }, data);
+    res.send("User updated successfully");
+  } catch (err) {
+    res.status(400).send("something wrong");
   }
 });
 
